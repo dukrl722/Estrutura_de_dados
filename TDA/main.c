d6de810b0a5f8f16b4ab6ef50c68beddbd65694d
Maikon <57468789+MR-dot-bin@users.noreply.github.com>
2020-03-15 16:16:21 -0300

    Update main.c

--- TDA/main.c
+++ TDA/main.c
@@ -105,12 +105,4 @@ int main ()
     
     }
     }
-
-    printf ("quadrado = %lf\n", calc_quadrado (3));
-    printf ("trapezio = %lf\n", calc_trapezio (2, 2, 2));
-    printf ("losango  = %lf\n", calc_losango (2, 2));
-    printf ("hexagono = %.1lf\n", calc_polig_regular (6, 2, 1.73));
-    printf ("juros simples = %lf\n", calc_juros_simp (2, 6, 4));
-    printf ("montante simples = %lf\n", calc_mont_simp (2, 4));
-    printf ("montante composto = %lf\n", calc_mont_comp (2, 6, 4));
 }
